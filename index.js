771b285fad9be16c22757f971f679d2bb9d796e8
christian <christian@enclave.vn>
2020-11-02 12:00:52 +0700

    report ver1, after manual

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const path = require('path');
 const fs = require('fs');
 const _ = require('lodash');
 
+//#############################
+let tagCounter = {};
+
 const csvPath = path.resolve(__dirname, 'vi_blog_train.csv');
 const pathOptions = {
     headers: true,
@@ -14,17 +17,33 @@ const pathOptions = {
     trim: true
 };
 
+const counter = (setArray) => {
+    setArray.forEach(tag => {
+        if (tagCounter[tag] >= 1) {
+            tagCounter[tag]++;
+        } else tagCounter[tag] = 1;
+    });
+}
+
 const handleError = (error) => console.log(error);
 const handleData = ({ title, tags }) => {
-    const rawArray = Array.from(JSON.parse(tags.replace(/["]/g, "?").replace(/[']/g, "\"").replace(/[?]/g, "'")));
+    const rawArray = Array.from(JSON.parse(tags.replace(/["]/g, "=").replace(/[']/g, "\"").replace(/[=]/g, "'")));
     const setArray = new Set(rawArray);
     const temp = {
         content: title,
         tags: Array.from(setArray)
     }
-    console.log(temp);
+    counter(setArray);
+    // console.info(temp);
 };
-const handleEOF = (rowCount) => console.log(`Total: ${rowCount} rows`);
+const handleEOF = (rowCount) => {
+    // console.info(`Total: ${rowCount} rows`);
+    // console.info(tagCounter);
+    fs.writeFile('vi_blog_train-report.json', JSON.stringify(tagCounter), function (err) {
+        if (err) throw err;
+        // console.info('Saved the report!');
+    });
+};
 
 fs.createReadStream(csvPath)
     .pipe(csv.parse(pathOptions))
