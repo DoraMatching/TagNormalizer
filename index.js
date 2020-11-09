b9245a3d2f55495a5ee98364f312b7b3c67e4a89
christian <christian@enclave.vn>
2020-11-09 14:50:41 +0700

    done

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const csv = require('@fast-csv/parse');
 const path = require('path');
 const fs = require('fs');
 const _ = require('lodash');
+const rules = require('./rules');
 
 //#############################
 let tagCounter = {};
+const after_fileName = `vi_blog_train-after.csv`;
+
+// Output stream
+fs.appendFileSync(after_fileName, `title,tags\n`); //add csv Header
 
 const csvPath = path.resolve(__dirname, 'vi_blog_train.csv');
 const pathOptions = {
@@ -25,6 +30,34 @@ const counter = (setArray) => {
     });
 }
 
+const tagNames = Object.keys(rules);
+const tagNamesLength = tagNames.length;
+
+const convertTag = (tag) => {
+    let result;
+    tagNames.forEach(tagName => {
+        rules[tagName].forEach(key => {
+            if (tag.includes(key)) {
+                console.log({ tag, tagName });
+                result = tagName;
+            };
+        })
+    });
+    return result;
+}
+
+const groupTags = (tags) => {
+    let results = [];
+    tags.forEach(tag => {
+        const result = convertTag(tag);
+        if (result)
+            results.push(result);
+    });
+    console.log(results);
+    results = results.filter(e => e !== null);
+    return results;
+}
+
 const handleError = (error) => console.log(error);
 const handleData = ({ title, tags }) => {
     const rawArray = Array.from(JSON.parse(tags.replace(/["]/g, "=").replace(/[']/g, "\"").replace(/[=]/g, "'")));
@@ -33,8 +66,17 @@ const handleData = ({ title, tags }) => {
         content: title,
         tags: Array.from(setArray)
     }
+    temp.tags = groupTags(temp.tags);
+    if (temp.tags.length === 0) return;
     counter(setArray);
-    // console.info(temp);
+
+    str = temp.tags.map(s => `'${s}'`).join(',');
+    str = `"[${str}]"`;
+
+    if (temp.content && temp.tags) {
+        fs.appendFile(after_fileName, `${temp.content},${str}\n`, err => console.error(err));
+        console.log(temp);
+    }
 };
 const handleEOF = (rowCount) => {
     // console.info(`Total: ${rowCount} rows`);
